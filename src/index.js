06b35f370c2cd83fa475b9d8b9e5d9281cec22d5
Giang Nguyen <thuygiang152@gmail.com>
2019-03-07 12:33:03 +0200

    delete some files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App8 from './App8';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App8 from "./App8";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App8 />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App8 />, document.getElementById("root"));
